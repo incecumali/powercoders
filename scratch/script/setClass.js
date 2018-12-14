8765a296dd5afa727178ea876a3ba8a2ad8d7642
Barin Kaya <barin.kaya@powercoders.org>
2018-12-14 16:39:56 +0100

    after code reducing

---
+++ scratch/script/setClass.js
@@ -0,0 +1,18 @@
+
+console.log('my code is running!');
+function greet(name) {
+  console.log('hello',+name);
+}
+
+
+
+function setClass(event,  type){
+  let el=document.getElementById('mypara');
+  el.className=type;
+}
+
+//setClassTip();
+//setClassWarning();
+//document.getElementById('war').onclick=setClass(event, 'warning');
+//document.getElementById('ti').onclick=setClassTip;
+//document.getElementById('nor').onclick=clearClass;
