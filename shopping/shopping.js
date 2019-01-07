c39ecac6c6833a72240f9a4bc402704bb101bd92
Barin Kaya <barin.kaya@powercoders.org>
2019-01-07 14:25:07 +0100

    we reach the content of the input test
    At this sage we reach content of the  text box and print it at he console

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,2 +1,13 @@
-let el= document.getElementById('item');
-el.value;
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    let inputBox= document.getElementById('item');
+    document.querySelector('button').addEventListener('click', function (event) {
+
+        console.log(inputBox.value);
+        // or
+        //console.log(document.getElementById('item').value); chaining
+
+    });
+
+})
