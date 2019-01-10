4780ee4dc0564d1477fb10a42037f56c9281e8a9
Barin Kaya <barin.kaya@powercoders.org>
2019-01-10 14:37:50 +0100

    desing to when user enter the iten
    It is a way of desiging  adding item when entered typed item

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -8,6 +8,48 @@ document.addEventListener('DOMContentLoaded', function (event) {
         // or
         //console.log(document.getElementById('item').value); chaining
 
+        let liItem= createNewListItem(inputBox.value);
+        let ulEl=document.querySelector('ul');
+        ulEl.appendChild(liItem);
+
+
+    });
+    document.querySelector('input').addEventListener('keyup', function (event ) {
+        if(event.key==='Enter' ){
+            let inputBox=document.getElementById('item');
+            let li= createNewListItem(inputBox.value);
+            let ulEl=document.querySelector('ul');
+            ulEl.appendChild(li);
+            // or document.querySelector('ul').appendChild(li);
+            console.log(event.key);
+
+        }
+
+
+
     });
 
-})
+});
+function createNewListItem(itemName){
+let listItem= document.createElement('li');
+let span=document.createElement('span');
+span.innerText=itemName;
+listItem.appendChild(span);
+
+
+let deleteButton=document.createElement('button')
+   let text= document.createTextNode('delete');
+deleteButton.addEventListener('click',function (event) {
+
+listItem.remove();
+
+});
+
+deleteButton.appendChild(text);
+listItem.appendChild(deleteButton);
+
+
+return listItem;
+}
+
+
