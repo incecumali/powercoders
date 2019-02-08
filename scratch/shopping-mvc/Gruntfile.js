9aaa358135f1313f40f1730d68725725436b8cbd
Barin Kaya <barin.kaya@powercoders.org>
2019-02-08 16:32:24 +0100

    it is copy property of grunt

--- scratch/shopping-mvc/Gruntfile.js
+++ scratch/shopping-mvc/Gruntfile.js
@@ -12,12 +12,30 @@ module.exports = function(grunt) {
                 src:['src/js/view.js', 'src/js/model.js', 'src/js/controller.js', 'src/js/shopping-list-item.js', 'src/js/ui.js'],
                 dest:'build/<%=pkg.name %>.min.js'
 
+            },
+
+
+        },
+        copy:{
+            html:{
+                src:'src/html/index.prod.html',
+                dest:'build/index.html'
+            },
+            static:{
+                files:[{
+                    expand:true,
+                    cwd:'src/static',
+                    src:'*.*',
+                    dest:'build/static',
+
+                }]
             }
         }
 
     });
     // load plugging that provides uglify
     grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     //default tasks
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'copy']);
 };
