52221bb2585eb8925e08481ec7beb1ec51483592
semaver <semaver34@gmail.com>
2019-02-25 15:09:52 +0100

    Creating lisz items

---
+++ shopping-react/src/ClearList.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class ClearList extends Component {
+    render() {
+        return <p>ClearList</p>
+    }
+}
+export default ClearList;
---
+++ shopping-react/src/ItemInput.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class ItemInput extends Component {
+    render() {
+        return <p>ItemInput</p>
+    }
+}
+export default ItemInput;
