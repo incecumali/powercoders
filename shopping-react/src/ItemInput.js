96336b145f50dafdf179fd84b65aeb6fa7a1eed7
semaver <semaver34@gmail.com>
2019-02-25 15:19:19 +0100

    Add widgets

--- shopping-react/src/ItemInput.js
+++ shopping-react/src/ItemInput.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 
 class ItemInput extends Component {
     render() {
-        return <p>ItemInput</p>
+        return (
+            <div>
+                <labe htmlFor = "item">Enter New Item </labe>
+                <input tylpe ="text" id = "quantity" size =" 8" placeholder="quantity"/>
+                <input type ="text" id = "item" placeholder="type something  to buy "/>
+                <button id="add">Add Item</button>
+
+            </div>
+        )
     }
 }
 export default ItemInput;
